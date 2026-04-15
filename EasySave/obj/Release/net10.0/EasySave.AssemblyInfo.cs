d000f2e35b6ca1baa438f749db95c3cd065701c1
Louison Masson <louison.masson@gmail.com>
2026-04-15 12:24:38 +0200

    core: add readme

--- EasySave/obj/Release/net10.0/EasySave.AssemblyInfo.cs
+++ EasySave/obj/Release/net10.0/EasySave.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasySave")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93b1bc7a312f496d154065552fd462c59a35ba18")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasySave")]
