4096e1dff7658e9be87f704c96c2e34ca19a9f2c
ewangerardin <120203870+ewangerardin@users.noreply.github.com>
2026-05-17 19:22:39 +0200

    Revert "fix: audit blockers - remove debug log, split ITransferCoordinator, E…"

--- GUI/ViewModels/MainViewModel.cs
+++ GUI/ViewModels/MainViewModel.cs
@@ -254,11 +254,15 @@ namespace EasySave.GUI.ViewModels
         {
             _service.Detach(this);
 
-            IStateFormatter stateFormatter = settings.LogFormat == LogFormat.Xml
-                ? new XmlStateFormatter()
-                : new JsonStateFormatter();
+            ILogFormatter formatter = settings.LogFormat == LogFormat.Json
+                ? new JsonLogFormatter()
+                : new XmlLogFormatter();
 
-            var logger = CreateLogWriter(settings);
+            IStateFormatter stateFormatter = settings.LogFormat == LogFormat.Json
+                ? new JsonStateFormatter()
+                : new XmlStateFormatter();
+
+            var logger = CreateLogWriter(settings, formatter);
             var encryptionService = CreateEncryptionService(settings);
             var guard = CreateBusinessSoftwareGuard(settings);
             var transferCoordinator = new TransferCoordinator(() => _settingsRepo.Load());
@@ -279,19 +283,10 @@ namespace EasySave.GUI.ViewModels
             StatusMessage = _loc.Get("menu_settings") + " " + _loc.Get("status_done");
         }
 
-        private ILogWriter CreateLogWriter(AppSettings settings)
+        private ILogWriter CreateLogWriter(AppSettings settings, ILogFormatter formatter)
         {
-            ILogWriter localLogger = settings.LogFormat switch
-            {
-                LogFormat.Xml => new EasyLogger(_logDir, new XmlLogFormatter()),
-                LogFormat.JsonAndXml => new CompositeLogWriter(new ILogWriter[]
-                {
-                    new EasyLogger(_logDir, new JsonLogFormatter()),
-                    new EasyLogger(_logDir, new XmlLogFormatter())
-                }),
-                _ => new EasyLogger(_logDir, new JsonLogFormatter())
-            };
-            var remoteLogger = new SocketLogWriter(settings.LogServerHost, settings.LogServerPort, new JsonLogFormatter());
+            var localLogger = new EasyLogger(_logDir, formatter);
+            var remoteLogger = new SocketLogWriter(settings.LogServerHost, settings.LogServerPort, formatter);
 
             return settings.LogTarget switch
             {
